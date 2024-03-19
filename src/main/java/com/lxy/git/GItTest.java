705e5154611ac44a2389cdf32beef3c1e04bf2cc
刘欣月 <Lxyforwork01@163.com>
2024-03-19 22:43:01 +0800

    hot-fix second commit

--- src/main/java/com/lxy/git/GItTest.java
+++ src/main/java/com/lxy/git/GItTest.java
@@ -8,6 +8,7 @@ public class GItTest {
         System.out.println("Hello Git!");
         System.out.println("Hello Git!33");
         System.out.println("Hello Git!4");
+        System.out.println("Hello Git!hot-fix test");
 
     }
 }
