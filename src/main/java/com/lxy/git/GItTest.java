e4e7fda0a39178390c86b0f3a58cf379624f90d7
刘欣月 <Lxyforwork01@163.com>
2024-03-19 22:22:09 +0800

    third commit

--- src/main/java/com/lxy/git/GItTest.java
+++ src/main/java/com/lxy/git/GItTest.java
@@ -6,7 +6,7 @@ package com.lxy.git;
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
+        System.out.println("Hello Git!33");
 
     }
 }
