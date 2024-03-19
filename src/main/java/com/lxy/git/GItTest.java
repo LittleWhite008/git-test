5689444df5ca570272b033fcc185ed11377725d0
刘欣月 <Lxyforwork01@163.com>
2024-03-19 18:01:53 +0800

    first commit

---
+++ src/main/java/com/lxy/git/GItTest.java
@@ -0,0 +1,9 @@
+package com.lxy.git;
+
+public class GItTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+        System.out.println("Hello Git!");
+
+    }
+}
