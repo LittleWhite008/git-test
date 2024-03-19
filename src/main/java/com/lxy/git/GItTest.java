d10c580c8e4d40b29339e16511cfff047c8d0781
刘欣月 <Lxyforwork01@163.com>
2024-03-19 22:20:03 +0800

    second commit

--- src/main/java/com/lxy/git/GItTest.java
+++ src/main/java/com/lxy/git/GItTest.java
@@ -1,5 +1,8 @@
 package com.lxy.git;
-
+/**
+ * @author lxy
+ * @date 2024/3/19 19:58
+ */
 public class GItTest {
     public static void main(String[] args) {
         System.out.println("Hello Git!");
